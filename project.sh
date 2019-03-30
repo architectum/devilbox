c95f5272148d9a08b1c1965f3bd2ad60ad836884
🚀 onegin <evgeniyivanets@gmail.com>
2019-03-31 01:21:42 +0200

    prj conf

--- project.sh
+++ project.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 PROJECT="${1}";
+
 mkdir -p "./data/www/${PROJECT}/htdocs"
-echo -e "<?php\nphpinfo();\n" >> "./data/www/${PROJECT}/htdocs/index.php"
+printf "<?php\n%s\n" "phpinfo();" >> "./data/www/${PROJECT}/htdocs/index.php"
+
+sudo chown -R ${USER} ./data/www/test
 sudo cp /etc/hosts /etc/_bcp_hosts
+
 sudo printf "\n%s %s\n" "127.0.0.1" "${PROJECT}.loc" >> /etc/hosts
